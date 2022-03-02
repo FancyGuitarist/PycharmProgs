7e7d33b3c345097db38ae5f6815deec3eb36510a
FancyGitarist <simf3010@gmail.com>
2022-03-02 13:31:51 -0500

    So it begins, again
    T'was the night before christmas

---
+++ hiiii.py
@@ -0,0 +1,27 @@
+import glob
+import matplotlib.pyplot as plt
+
+
+
+listex, listey = [], []
+for i in glob.glob("Lab5/*.csv"):
+    file = open(i, 'r')
+    results = list(file)
+    file.close()
+    x1, y1 = [], []
+    results = results[3:]
+    for i in results:
+        result = i.replace('\n', '').split(',')
+        x1.append(float(result[0]))
+        y1.append(float(result[1]))
+    listex.append(x1)
+    listey.append(y1)
+print(len(listex), len(listey))
+
+#fig1, fig2, fig3, fig4, fig5, fig6 = plt.figure(6)
+for i in range(6):
+    plt.figure()
+    plt.plot(listex[i], listey[i])
+    plt.savefig(f"Graph{i}")
+plt.show()
+print("Hello General Kenobi")
